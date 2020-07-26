cc17af593374ce42e9fcce416715ef646b87a323
Rominos111 <romain.theodet@hotmail.fr>
2020-07-27 00:06:17 +0200

    change: Require en cascade

--- actions/employees/index.php
+++ actions/employees/index.php
@@ -1,11 +1,6 @@
 <?php
 
-session_start();
-
-# Affichage des message de debug
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", 1);
-error_reporting(E_ALL);
+require "__php__";
 
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
--- index.php
+++ index.php
@@ -1,9 +1,1 @@
 <?php
-
-session_start();
-
-# Affichage des message de debug
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", 1);
-error_reporting(E_ALL);
-
