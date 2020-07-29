ae16c7dd38ad3a8cfa4d654955a9a683586ce929
Rominos111 <romain.theodet@hotmail.fr>
2020-07-30 00:25:08 +0200

    fix: include -> require

--- actions/employees/index.php
+++ actions/employees/index.php
@@ -4,24 +4,21 @@ require "__php__";
 
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
-        include_once "operations/retrieve.php";
+        require_once "operations/retrieve.php";
         break;
 
     case "POST":
-        include_once "operations/create.php";
+        require_once "operations/create.php";
         break;
 
-    case "PUT":
     case "PATCH":
-        include_once "operations/update.php";
+        require_once "operations/update.php";
         break;
 
     case "DELETE":
-        include_once "operations/delete.php";
+        require_once "operations/delete.php";
         break;
 
     default:
         break;
 }
-
-echo "employés";
