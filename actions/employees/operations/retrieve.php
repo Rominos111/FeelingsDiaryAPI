bfce4be385c5d2137956a9a03cc5d4d982ed3c53
Rominos111 <romain.theodet@hotmail.fr>
2020-08-20 22:40:41 +0200

    test: Json

--- actions/employees/operations/retrieve.php
+++ actions/employees/operations/retrieve.php
@@ -1,1 +1,15 @@
 <?php
+
+Database::connect();
+
+if (empty($_GET)) {
+    $test = array("abc", "def");
+
+    $res = array("content" => $test);
+
+    echo json_encode($res);
+}
+else {
+    echo "get";
+}
+
