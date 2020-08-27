4ff8776fec031db018f3e3cc108438f206e9d090
Rominos111 <romain.theodet@hotmail.fr>
2020-08-28 00:44:03 +0200

    new: Vérifications connexion

--- actions/auth/connect.php
+++ actions/auth/connect.php
@@ -1,44 +1,52 @@
 <?php
 
+$_ENV["ALLOWED_METHODS"] = "POST";
 require_once "__php__";
 
-require_once "utils/random.php";
 require_once "objects/User.php";
 require_once "shared/JWT.php";
-
-if ($_SERVER["REQUEST_METHOD"] != "POST") {
-    (new Response(
-        ResponseCode::METHOD_NOT_ALLOWED,
-        array("method" => $_SERVER["REQUEST_METHOD"]),
-        1,
-        "Method not allowed, like using POST in a GET-only request"
-    ))->send();
-}
 
 if (isset($_POST)) {
     if (!empty($_POST["password"]) && is_string($_POST["password"])) {
         if (!empty($_POST["username"]) && is_string($_POST["username"])) {
-            Database::connect();
+            $user = User::getByUsername($_POST["username"]);
 
-            $canConnect = User::canConnect($_POST["username"], $_POST["password"]);
+            if (!is_null($user)) {
+                $canConnect = User::canConnect($_POST["username"], $_POST["password"]);
 
-            if ($canConnect) {
-                $uid = User::getByUsername($_POST["username"])->getId();
+                if ($canConnect) {
+                    $uid = User::getByUsername($_POST["username"])->getId();
 
-                $rft = JWT::getToken($uid, JWT::TOKEN_REFRESH);
-                $rqt = JWT::getToken($uid, JWT::TOKEN_REQUEST);
+                    $rft = JWT::getToken($uid, JWT::TOKEN_REFRESH);
+                    $rqt = JWT::getToken($uid, JWT::TOKEN_REQUEST);
 
-                (new Response(200, array(
-                    "refresh_token" => $rft,
-                    "request_token" => $rqt
-                )))->send();
+                    Response::builder()
+                        ->setHttpCode(ResponseCode::OK)
+                        ->setContent(array(
+                            "refreshToken" => $rft,
+                            "requestToken" => $rqt
+                        ))
+                        ->send();
+                }
+                else {
+                    Response::builder()
+                        ->setHttpCode(ResponseCode::UNAUTHORIZED)
+                        ->setMessage("Wrong password")
+                        ->send();
+                }
+            }
+            else {
+                Response::builder()
+                    ->setHttpCode(ResponseCode::NOT_FOUND)
+                    ->setMessage("User not found")
+                    ->send();
             }
         }
         else {
-            (Response::missingArguments("username"))->send();
+            Response::missingArguments("username")->send();
         }
     }
     else {
-        (Response::missingArguments("password"))->send();
+        Response::missingArguments("password")->send();
     }
 }
