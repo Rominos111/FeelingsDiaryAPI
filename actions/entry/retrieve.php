73d20137e0c37aa8ee5777c54c92bcbeae6dbc04
Rominos111 <romain.theodet@hotmail.fr>
2020-08-28 00:44:25 +0200

    new: Token dans get entry

--- actions/entry/retrieve.php
+++ actions/entry/retrieve.php
@@ -1,25 +1,51 @@
 <?php
 
+$_ENV["ALLOWED_METHODS"] = "GET";
 require_once "__php__";
-require_once "objects/Entry.php";
 
-if ($_SERVER["REQUEST_METHOD"] != "GET") {
-    $response = new Response(ResponseCode::METHOD_NOT_ALLOWED, array("method" => $_SERVER["REQUEST_METHOD"]));
-    $response->send(1, "Method not allowed, for example using POST on a GET-only file");
-    exit();
-}
+require_once "objects/Entry.php";
+require_once "shared/JWT.php";
 
 if (isset($_GET)) {
-    if (isset($_GET["id"]) && !empty($_GET["id"])) {
-        if (is_numeric($_GET["id"])) {
-            Database::connect();
-
-            $entry = Entry::getById($_GET["id"]);
-            $response = new Response(ResponseCode::OK, $entry->toArray());
-            $response->send();
+    if (!empty($_GET["token"]) && is_string($_GET["token"])) {
+        $invalid = JWT::isInvalidToken($_GET["token"]);
+        if (!$invalid) {
+            if (!empty($_GET["id"]) && is_numeric($_GET["id"])) {
+                $entry = Entry::getById($_GET["id"]);
+                if (!is_null($entry)) {
+                    if ($entry->getUserID() === JWT::getUserID($_GET["token"])) {
+                        Response::builder()
+                            ->setContent($entry->toArray())
+                            ->send();
+                    }
+                    else {
+                        Response::builder()
+                            ->setHttpCode(ResponseCode::UNAUTHORIZED)
+                            ->setMessage("Entry not found")
+                            ->setCustomCode(11)
+                            ->send();
+                    }
+                }
+                else {
+                    Response::builder()
+                        ->setHttpCode(ResponseCode::NOT_FOUND)
+                        ->setMessage("Entry not found")
+                        ->send();
+                }
+            }
+            else {
+                Response::missingArguments("id")->send();
+            }
+        }
+        else {
+            Response::builder()
+                ->setHttpCode(ResponseCode::UNAUTHORIZED)
+                ->setMessage("Invalid token")
+                ->setCustomCode($invalid)
+                ->send();
         }
     }
     else {
-        echo "list";
+        Response::missingArguments("token")->send();
     }
 }
