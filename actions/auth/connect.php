9862e681a3cc7bc6cc4a3e767adb66d6902c4d0c
Rominos111 <romain.theodet@hotmail.fr>
2020-08-29 14:22:39 +0200

    refactor: Suppression des if en cascade

--- actions/auth/connect.php
+++ actions/auth/connect.php
@@ -4,49 +4,56 @@ $_ENV["ALLOWED_METHODS"] = "POST";
 require_once "__php__";
 
 require_once "objects/User.php";
-require_once "shared/JWT.php";
 
-if (isset($_POST)) {
-    if (!empty($_POST["password"]) && is_string($_POST["password"])) {
-        if (!empty($_POST["username"]) && is_string($_POST["username"])) {
-            $user = User::getByUsername($_POST["username"]);
-
-            if (!is_null($user)) {
-                $canConnect = User::canConnect($_POST["username"], $_POST["password"]);
-
-                if ($canConnect) {
-                    $uid = User::getByUsername($_POST["username"])->getId();
-
-                    $rft = JWT::getToken($uid, JWT::TOKEN_REFRESH);
-                    $rqt = JWT::getToken($uid, JWT::TOKEN_REQUEST);
-
-                    Response::builder()
-                        ->setHttpCode(ResponseCode::OK)
-                        ->setContent(array(
-                            "refreshToken" => $rft,
-                            "requestToken" => $rqt
-                        ))
-                        ->send();
-                }
-                else {
-                    Response::builder()
-                        ->setHttpCode(ResponseCode::UNAUTHORIZED)
-                        ->setMessage("Wrong password")
-                        ->send();
-                }
-            }
-            else {
-                Response::builder()
-                    ->setHttpCode(ResponseCode::NOT_FOUND)
-                    ->setMessage("User not found")
-                    ->send();
-            }
-        }
-        else {
-            Response::missingArguments("username")->send();
-        }
-    }
-    else {
-        Response::missingArguments("password")->send();
-    }
+// Vérification validité password
+if (empty($_POST["password"])) {
+    Response::missingArguments("password")->send();
 }
+
+// Vérification validité password
+if (!is_string($_POST["password"])) {
+    Response::wrongDataType("password", $_POST["password"], "")->send();
+}
+
+// Vérification validité username
+if (empty($_POST["username"])) {
+    Response::missingArguments("username")->send();
+}
+
+// Vérification validité username
+if (!is_string($_POST["username"])) {
+    Repsonse::wrongDataType("username", $_POST["username"], "")->send();
+}
+
+$user = User::getByUsername($_POST["username"]);
+
+// Vérification existence user
+if (is_null($user)) {
+    Response::builder()
+        ->setHttpCode(ResponseCode::NOT_FOUND)
+        ->setMessage("User not found")
+        ->send();
+}
+
+$canConnect = User::canConnect($_POST["username"], $_POST["password"]);
+
+// Vérification validité password
+if (!$canConnect) {
+    Response::builder()
+        ->setHttpCode(ResponseCode::UNAUTHORIZED)
+        ->setMessage("Wrong password")
+        ->send();
+}
+
+$uid = User::getByUsername($_POST["username"])->getId();
+
+$rft = JWT::getToken($uid, JWT::TOKEN_REFRESH);
+$rqt = JWT::getToken($uid, JWT::TOKEN_REQUEST);
+
+Response::builder()
+    ->setHttpCode(ResponseCode::OK)
+    ->setPayload(array(
+        "refreshToken" => $rft,
+        "requestToken" => $rqt
+    ))
+    ->send();
--- actions/entry/retrieve.php
+++ actions/entry/retrieve.php
@@ -4,48 +4,34 @@ $_ENV["ALLOWED_METHODS"] = "GET";
 require_once "__php__";
 
 require_once "objects/Entry.php";
-require_once "shared/JWT.php";
 
-if (isset($_GET)) {
-    if (!empty($_GET["token"]) && is_string($_GET["token"])) {
-        $invalid = JWT::isInvalidToken($_GET["token"]);
-        if (!$invalid) {
-            if (!empty($_GET["id"]) && is_numeric($_GET["id"])) {
-                $entry = Entry::getById($_GET["id"]);
-                if (!is_null($entry)) {
-                    if ($entry->getUserID() === JWT::getUserID($_GET["token"])) {
-                        Response::builder()
-                            ->setPayload($entry->toArray())
-                            ->send();
-                    }
-                    else {
-                        Response::builder()
-                            ->setHttpCode(ResponseCode::UNAUTHORIZED)
-                            ->setMessage("Entry not found")
-                            ->setCustomCode(11)
-                            ->send();
-                    }
-                }
-                else {
-                    Response::builder()
-                        ->setHttpCode(ResponseCode::NOT_FOUND)
-                        ->setMessage("Entry not found")
-                        ->send();
-                }
-            }
-            else {
-                Response::missingArguments("id")->send();
-            }
-        }
-        else {
-            Response::builder()
-                ->setHttpCode(ResponseCode::UNAUTHORIZED)
-                ->setMessage("Invalid token")
-                ->setCustomCode($invalid)
-                ->send();
-        }
-    }
-    else {
-        Response::missingArguments("token")->send();
-    }
+JWTResponse::checkRequestToken($_GET["token"]);
+
+if (empty($_GET["id"])) {
+    Response::missingArguments("id")->send();
 }
+
+if (!is_numeric($_GET["id"])) {
+    Response::wrongDataType("id", $_GET["id"], 0)->send();
+}
+
+$entry = Entry::getById($_GET["id"]);
+
+if (is_null($entry)) {
+    Response::builder()
+        ->setHttpCode(ResponseCode::NOT_FOUND)
+        ->setMessage("Entry not found")
+        ->send();
+}
+
+// Mieux qu'un FORBIDDEN ?
+if ($entry->getUserID() !== JWT::getUserID($_GET["token"])) {
+    Response::builder()
+        ->setHttpCode(ResponseCode::NOT_FOUND)
+        ->setMessage("Entry not found")
+        ->send();
+}
+
+Response::builder()
+    ->setPayload($entry->toArray())
+    ->send();
