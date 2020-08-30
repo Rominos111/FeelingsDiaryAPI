39c6412b1834e6e3035c1d005c5e6cb266604775
Rominos111 <romain.theodet@hotmail.fr>
2020-08-30 12:21:26 +0200

    new: Création entrée

--- actions/entry/create.php
+++ actions/entry/create.php
@@ -1,1 +1,34 @@
 <?php
+
+$_ENV["EXPECTED"] = array(
+    "methods" => "POST",
+    "args" => array(
+        "content" => "",
+        "token" => ""
+    )
+);
+
+require_once "__php__";
+
+require_once "objects/Entry.php";
+
+JWTResponse::checkRequestToken($_POST["token"]);
+
+$id = null;
+
+try {
+    $id = Entry::create($_POST["content"], JWT::getUserID($_POST["token"]));
+}
+catch (UserNotFoundException $e) {
+    Response::builder()
+        ->setMessage("You don't exist")
+        ->setHttpCode(ResponseCode::UNPROCESSABLE_ENTITY)
+        ->send();
+}
+
+Response::builder()
+    ->setHttpCode(ResponseCode::CREATED)
+    ->setMessage("Entry created")
+    ->setPayload(array(
+        "id" => $id
+    ))->send();
